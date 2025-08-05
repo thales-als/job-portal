5ae8671c46c3c3defe72aacbb1cc58cca5ee878c
thales-als <thalessouza.dev@gmail.com>
2025-08-05 08:15:20 -0300

    build: creates spring project

---
+++ src/main/java/br/com/thaleslima/job_portal/JobPortalApplication.java
@@ -0,0 +1,13 @@
+package br.com.thaleslima.job_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thaleslima/job_portal/JobPortalApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.thaleslima.job_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
